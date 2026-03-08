24df598f6b24d99c33d6e8ea52dcde28af63a7c1
Ismael Jara <ismaelvicentejara@gmail.com>
2026-03-07 19:06:24 -0500

    Subir archivo java

---
+++ reserva-asientos-cine.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ReservaCine {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] asientos = new int[3][4]; // matriz 3x4
+
+        System.out.println("Ingrese fila (0 a 2): ");
+        int f = sc.nextInt();
+
+        System.out.println("Ingrese columna (0 a 3): ");
+        int c = sc.nextInt();
+
+        // reservar asiento
+        asientos[f][c] = 1;
+
+        System.out.println("Estado de la sala:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(asientos[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
